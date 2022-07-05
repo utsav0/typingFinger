0704bc761780f7f9b3a5800c6e71664c014e9397
utsav <meenautsav1612@gmail.com>
2022-07-05 15:27:45 +0530

    signup page created

--- TypingFinger/views.py
+++ TypingFinger/views.py
@@ -1,7 +1,7 @@
 from ctypes.wintypes import PDWORD
 from distutils.command import check
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from requests import request
 from urllib3 import Retry
 
@@ -19,8 +19,8 @@ def forgotpwd(request):
     return render(request, "forgotpwd.html")
 
 def checkCredentials(email, pwd):
-    return False
-    
+    return True
+
 def login(request):
     email = request.POST.get("email", None)
     pwd = request.POST.get("pwd", None)
@@ -28,11 +28,21 @@ def login(request):
     if email != None and pwd != None:
         status = checkCredentials(email, pwd)
         if status == True:
-            practice()
+            return redirect("/practice", {1:"one"})
         elif status == False:
-            return render(request, "login.html", {"warning":"Credentials did not match!"})
+            return render(request, "login.html", {"message":"Credentials did not match!"})
     else:
-        return render(request, "login.html", {"warning":""})
+        return render(request, "login.html", {"message":""})
+
+def addNewUser(request):
+    email = request.POST.get("email", None)
+    pwd = request.POST.get("pwd", None)
+    if email != None and pwd != None:
+        #add the new user in database
+        return render(request, "login.html", {"message":"Signup successful! Please login here:", "msgColor":"green"})
+    else:
+        return HttpResponse("Ran Into Unknown Error!")
+    
 
 
 
